ea67a3da35b4b8be7b1940665e218e1291bdc2a3
Robert Fine <robertfine4631@icloud.com>
2015-05-18 16:28:32 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function updateClock() {
+  var now = new Date();
+ 
+   var hours = now.getHours();
+   var minutes = now.getMinutes();
+   var seconds = now.getSeconds();
+ 
+    if (hours < 10) {
+      hours = "0" + hours;
+      }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+      }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+      }
+ 
+  var elem = document.getElementById('time');
+ 
+  elem.innerHTML = '#' + hours + '' + minutes + '' + seconds;
+}
+
+window.setInterval(updateClock, 200);
